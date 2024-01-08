6788f849c7497541fd0112edb5a3287bdbe3e1f6
KareemSaysHi <kareemjaber8@gmail.com>
2024-01-08 11:03:32 -0500

    8 core threading works

--- fastcollatz/main.c
+++ fastcollatz/main.c
@@ -6,59 +6,73 @@
 #include <wait.h>
 #include <time.h>
 
-clock_t startClock;
-clock_t endClock;
-double cpuSecondsConsumed;
+#define MAX_COMMAND_LENGTH 1000  
+
+FILE *fptr;
+
+int check_power_of_two(int numtocheck) {
+    while (numtocheck != 1 ) {
+        if (numtocheck % 2 != 0) return 0;
+        numtocheck /= 2;
+    }
+}
 
 int main(int argc, char **argv) {
-    size_t input;
-    size_t forking = 0;
+    size_t input1, input2;
+    size_t searchupto = 0;
+    int numforks = 0;
     size_t result2 = 0;
-    FILE *fptr;
-    
-    if(argc != 2) {
+    char **command = NULL;
+    pid_t pid;
+
+
+    /* check inputs*/
+    if(argc != 3) {
         printf("unexpected number of arguments\n");
         return -1;
     }
+    
+    searchupto = 1 << (atoi(argv[1]));
+    numforks = atoi(argv[2]);
 
-    forking = atoi(argv[1]);
-    if (forking == 0) {
-        startClock = clock();
-        system("./subcollatz file1 0 50000000");
-        system("./subcollatz file2 50000000 100000000");
-        endClock = clock();
-        system("cat file* > finalfile");
-        
-        cpuSecondsConsumed = ((double)(endClock - startClock)) / CLOCKS_PER_SEC;
-        fptr = fopen("finalfile", "a");
-        fprintf(fptr, "Program took %f seconds without forking", cpuSecondsConsumed);
-        fclose(fptr);
-        return 0;
+    
+    printf("%zu\n", searchupto);
+    printf("%d\n", numforks);
 
-    } else  {
-        startClock = clock();
-        pid_t pid;
+    if (!(check_power_of_two(numforks)) || (numforks > 8))   {
+        printf("you need a forking number of 1, 2, 4, 8\n");
+        return -1;
+    }
+    
+
+    /* format the forking */
+
+    command = malloc(numforks * sizeof(char*));
+
+    for (int i = 0; i < numforks; i++) {
+        command[i] = malloc(MAX_COMMAND_LENGTH);
+        sprintf(command[i], "./subcollatz file%d %zu %zu", i, (searchupto*i)/numforks, (searchupto*(i+1))/numforks);
+        printf("./subcollatz file%d %zu %zu\n", i, (searchupto*i)/numforks, (searchupto*(i+1))/numforks);
+
+    }
+
+    /* and now we do parallel */
+    for (int i = 0; i < numforks; i++) {
         pid = fork();
 
-        if (pid == 0) {//child branch
-            system("./subcollatz file1 0 50000000"); 
+        if (pid == 0) {
+            //printf("running command %s\n", command[i]);
+            system(command[i]);
+            printf("done with %d\n", i);
             exit(0);
         }
+    }
 
-        else { //parent branch
-            system("./subcollatz file2 50000000 100000000");
-            wait(NULL);
-        }
-    endClock = clock();
+    for (int i = 0; i < numforks; i++) {
+        wait(NULL);
+    }
     
     system("cat file* > finalfile");
 
-    cpuSecondsConsumed = ((double)(endClock - startClock)) / CLOCKS_PER_SEC;
-    fptr = fopen("finalfile", "a");
-    fprintf(fptr, "Program took %f seconds with forking", cpuSecondsConsumed);
-    fclose(fptr);
-
     return 0;
-    
-    }
 }
